dc261e96feb4c5e9ffaaa7b2ffb8e43e4a8bf15a
6peng <6peng@proton.me>
2022-07-06 22:40:39 +0800

    add hellworld multimedia

--- main.cpp
+++ main.cpp
@@ -12,11 +12,35 @@ $ QT_LOGGING_RULES="*.debug=true" ./helloworld
 #else
 #include <QtCore/QCoreApplication>
 #include <QDebug>
+#include <QAudioDeviceInfo>
+
+QAudioDeviceInfo getAudioDevice(QString name) {
+  QAudioDeviceInfo device;
+  QList<QAudioDeviceInfo> devices =
+    QAudioDeviceInfo::availableDevices(QAudio::AudioInput);
+  for(int i = 0; i < devices.size(); ++i) {
+    qDebug() << "input Device name: " << devices.at(i).deviceName();
+    if(devices.at(i).deviceName() == name) {
+      device = devices.at(i);
+      break;
+    }
+  }
+  return device;
+}
 
 int main(int argc, char *argv[])
 {
   QCoreApplication a(argc, argv);
   qDebug() << "Hello World!";
+
+  const auto deviceInfos = QAudioDeviceInfo::availableDevices(QAudio::AudioOutput);
+  for (const QAudioDeviceInfo &deviceInfo : deviceInfos)
+    qDebug() << "output Device name: " << deviceInfo.deviceName();
+
+  QAudioDeviceInfo myDevice = getAudioDevice("default");
+  if(myDevice.isNull()) {
+    qCritical("Device not found");
+  }
   return a.exec(); // .exec starts QApplication and related GUI, this line starts 'event loop'    
 }
 #endif
